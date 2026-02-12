b40259ae35631d81d5e10bf7169378ccbe6a5d38
Sai Surya <tnvssuryaprasad06@gmail.com>
2026-02-12 19:11:33 +0530

    Deleted all files and folders

--- backend/src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/auth');
-const { updateUser, deleteUser } = require('../controllers/users');
-
-router.put('/:userId', authMiddleware, updateUser);
-router.delete('/:userId', authMiddleware, deleteUser);
-
-module.exports = router;
